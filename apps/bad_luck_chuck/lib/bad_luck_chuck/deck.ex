a40134f5bd498599e4e5b3fad7edbf0a4ba07c68
David Bowers <dbowers.github@gmail.com>
2017-04-26 15:42:41 -0600

    add functionality to deck to detect runs and books

--- apps/bad_luck_chuck/lib/bad_luck_chuck/deck.ex
+++ apps/bad_luck_chuck/lib/bad_luck_chuck/deck.ex
@@ -2,13 +2,9 @@ defmodule BadLuckChuck.Deck do
   @moduledoc """
   """
   alias BadLuckChuck.Card
+  alias BadLuckChuck.Sequencer
 
-  import BadLuckChuck.Pile, only: [
-    sort: 2,
-    top: 2,
-    same_rank?: 1,
-    same_suit?: 1
-  ]
+  import BadLuckChuck.Pile, only: [same_rank?: 1, same_suit?: 1]
 
   def minimum_cards?(cards) when is_list(cards) do
      Enum.count(cards) >= 3
@@ -18,13 +14,12 @@ defmodule BadLuckChuck.Deck do
     minimum_cards?(cards) && same_rank?(cards)
   end
 
-  # def run?(cards) do
-  #   cond do
-  #     !minimum_cards?(cards) -> false       # A run must have at least 3 cards
-  #     !same_suit?(cards) -> false           # The cards must have the same suit
-  #     sequential?(cards) -> true            # Card ranks must be sequential
-  #     sequential_high_ace?(cards) -> true   # An Ace can have a high or low rank
-  #     true -> false                         # These cards are not a run
-  #   end
-  # end
+  def run?(cards) do
+    cond do
+      !minimum_cards?(cards) -> false       # A run must have at least 3 cards
+      !same_suit?(cards) -> false           # The cards must have the same suit
+      Sequencer.sequential?(cards) -> true  # Card ranks must be sequential
+      true -> false                         # These cards are not a run
+    end
+  end
 end
