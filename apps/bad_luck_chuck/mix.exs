c68e290c69dd0fe7b64314af92ab97d601d9a4c1
David Bowers <dbowers.github@gmail.com>
2017-04-26 11:06:13 -0600

    add entry point

--- apps/bad_luck_chuck/mix.exs
+++ apps/bad_luck_chuck/mix.exs
@@ -19,7 +19,7 @@ defmodule BadLuckChuck.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: [:logger], mod: {BadLuckChuck, []}]
   end
 
   # Dependencies can be Hex packages:
