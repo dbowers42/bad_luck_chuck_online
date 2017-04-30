60770944744d4639f272bd1fb87dc75ac1c9d613
David Bowers <dbowers.github@gmail.com>
2017-04-29 22:23:37 -0600

    add a Board module to keep track of decks of cards in play on a board

--- apps/bad_luck_chuck/lib/bad_luck_chuck/board.ex
+++ apps/bad_luck_chuck/lib/bad_luck_chuck/board.ex
@@ -1,4 +1,27 @@
 defmodule BadLuckChuck.Board do
   @moduledoc """
+    The BadLuckChuck.Board module provides functions to manage decks of cards
+    that have been played on a board
   """
+  use GenServer
+
+  def start_link(board_id) when is_atom(board_id) do
+    GenServer.start_link(__MODULE__, %{decks: []}, name: board_id)
+  end
+
+  def decks(deck) do
+    GenServer.call(deck, :decks)
+  end
+
+  def add_deck(deck) do
+    GenServer.cast(__MODULE__, {:add_deck, deck})
+  end
+
+  def handle_call(:decks, _from, state) do
+    {:reply, state.decks, state}
+  end
+
+  def handle_cast({:add_deck, deck}, state) do
+    {:noreply, %{state | decks: [deck | state.decks]}}
+  end
 end
