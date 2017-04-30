6cac53e5a5415989ffb61555548fb17da9d07f81
David Bowers <dbowers.github@gmail.com>
2017-04-29 20:56:45 -0600

    add a Hand module to keep track of the cards in a players hand

--- apps/bad_luck_chuck/lib/bad_luck_chuck/hand.ex
+++ apps/bad_luck_chuck/lib/bad_luck_chuck/hand.ex
@@ -1,5 +1,30 @@
 defmodule BadLuckChuck.Hand do
   @moduledoc """
+    The BadLuckChuck.Hand module provides functions to keep track of what cards
+    are in a players hand.
   """
+  use GenServer
+
   import BadLuckChuck.Pile, only: [sort: 2]
+
+  def start_link(hand_id) when is_atom(hand_id) do
+    GenServer.start_link(__MODULE__, %{cards: []}, name: hand_id)
+  end
+
+  def cards(hand) do
+    IO.inspect hand
+    GenServer.call(hand, :cards)
+  end
+
+  def add_card(hand, card) do
+    GenServer.cast(hand, {:add_card, card})
+  end
+
+  def handle_call(:cards, _from, state) do
+    {:reply, state.cards, state}
+  end
+
+  def handle_cast({:add_card, card}, state) do
+    {:noreply, %{state | cards: [card| state.cards]}}
+  end
 end
