ec0ec645da9702b5feeb1449e2f9952bdca6b7af
David Bowers <dbowers.github@gmail.com>
2017-04-29 22:48:23 -0600

    add a supervision tree to the Game module

--- apps/bad_luck_chuck/lib/bad_luck_chuck/game.ex
+++ apps/bad_luck_chuck/lib/bad_luck_chuck/game.ex
@@ -1,4 +1,28 @@
 defmodule BadLuckChuck.Game do
   @moduledoc """
   """
+
+  alias BadLuckChuck.{PlayerSupervisor, DeckSupervisor}
+  alias BadLuckChuck.{Player, Board, DrawPile}
+
+  use GenServer
+
+  def start_link(game_id) when is_atom(game_id) do
+    GenServer.start_link(__MODULE__, %{}, name: game_id)
+  end
+
+  def init(state) do
+    import Supervisor.Spec
+
+    children = [
+      supervisor(PlayerSupervisor, []),
+      supervisor(DeckSupervisor, []),
+      worker(Board, [:active_board]),
+      worker(DrawPile, [])
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
+
+    {:ok, state}
+  end
 end
